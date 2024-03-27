6837b32e14f23b625ba26afd72f80890166d866d
lenovo <lenovo@DESKTOP-IOQ7J5S>
2024-03-27 20:30:24 +0530

    Updated Programs

---
+++ MavenLearning/src/main/java/sample/SampleProgram.java
@@ -0,0 +1,15 @@
+package sample;
+
+public class SampleProgram {
+
+	public  void print()
+	{
+		System.out.println("Hello World");
+	}
+	
+	public static void main(String[] args) {
+		SampleProgram obj= new SampleProgram();
+		obj.print();
+	}
+
+}
